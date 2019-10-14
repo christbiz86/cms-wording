b5b37aafd4e906ca4a09ad7b3f4ed6e69b6c2c99
christbiz86 <christ.biz@gmail.com>
2019-10-14 09:58:12 +0700

    add cron file

---
+++ CronJson.php
@@ -0,0 +1,6 @@
+<?php
+
+//echo 'scp .\api\controllers\apibed100.php root@10.16.5.162:/var/www/custinfo.smartfren.com/application/api/controllers/';
+
+// 0 0 * * * /usr/bin/rsync -a /origin /destination
+
