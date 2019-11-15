bd77e4b0d6fa0f7a4030ffeb184c94da6497e793
Christian K <christ.biz@gmail.com>
2019-11-15 15:40:44 +0700

    change url css

--- application/views/components/header.php
+++ application/views/components/header.php
@@ -6,7 +6,7 @@
 <!-- Font Awesome -->
 <link rel="stylesheet" href="<?=site_url()?>assets/plugins/fontawesome-free/css/all.min.css">
 <!-- Ionicons -->
-<link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+<link rel="stylesheet" href="<?=site_url()?>assets/dist/css/ionicons.min.css">
 <!-- Tempusdominus Bbootstrap 4 -->
 <link rel="stylesheet" href="<?=site_url()?>assets/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
 <!-- iCheck -->
@@ -22,6 +22,6 @@
 <!-- summernote -->
 <link rel="stylesheet" href="<?=site_url()?>assets/plugins/summernote/summernote-bs4.css">
 <!-- Google Font: Source Sans Pro -->
-<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+<link href="<?=site_url()?>assets/dist/css/css.css" rel="stylesheet">
 <!-- DataTables -->
 <link rel="stylesheet" href="<?=site_url()?>assets/plugins/datatables-bs4/css/dataTables.bootstrap4.css">
