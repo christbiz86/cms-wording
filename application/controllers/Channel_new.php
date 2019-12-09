6c0a19e6ce84242dc258e2030dff06d7afc50a39
Christian K <christ.biz@gmail.com>
2019-12-09 10:04:35 +0700

    upd channel new

--- application/controllers/Channel_new.php
+++ application/controllers/Channel_new.php
@@ -26,28 +26,26 @@ class Channel_new extends Wordingabstract{
 	}
 
 	public function create(){
-		$count = 0;
 		$object = $this->uri->segment(2);
 		$file = $this->getJsonFile();
 		$id = $_POST['id'];
 		$group = $_POST['group'];
+		$list[0] = ((object) $_POST);
+		unset($list[0]->id);
+		unset($list[0]->group);
 		if(isset($file->$object->$id->$group)){
-			$list[$id] = ((object) $_POST);
-			unset($list[$id]->id);
-			unset($list[$id]->group);
 			$file->$object->$id->$group = array_merge($list,$file->$object->$id->$group);
 		} else {
-			$list[0] = ((object) $_POST);
-			unset($list[0]->id);
-			unset($list[0]->group);
 			$file->$object->$id->$group = $list;
 		}
+//		var_dump($file->$object->$id->$group);
 		$this->updateJsonFile(json_encode($file));
 		redirect(site_url('/wording/'.$object));
 	}
 
 	public function update(){
 		$object = $this->uri->segment(2);
+		$id_post = $_POST['id'];
 		$id = $this->uri->segment(4);
 		$id1 = $this->uri->segment(5);
 		$id2 = $this->uri->segment(6);
@@ -66,7 +64,8 @@ class Channel_new extends Wordingabstract{
 		$id1 = $this->uri->segment(5);
 		$id2 = $this->uri->segment(6);
 		$file = $this->getJsonFile();
-		unset($file->$object->$id->$id1->$id2);
+		unset($file->$object->$id->$id1[$id2]);
+		$file->$object->$id->$id1 = array_values($file->$object->$id->$id1);
 		$this->updateJsonFile(json_encode($file));
 		redirect(site_url('/wording/'.$object));
 	}
